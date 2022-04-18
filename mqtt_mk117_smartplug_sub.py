f921ee05add39588bdb2ff029c8ca2c5c8207ca3
pjdenny <99647710+pjdenny@users.noreply.github.com>
2022-04-18 14:37:35 +0530

    Add files via upload
    python code subscribing to public broker broker.emqx.io for smart plug payload from mokosmart device  MK117-3e04 with mqtt topic as "MK117-3e04/04/device_to_app/#"

---
+++ mqtt_mk117_smartplug_sub.py
@@ -0,0 +1,27 @@
+# subscriber.py
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    # subscribe, which need to put into on_connect
+    # if reconnect after losing the connection with the broker, it will continue to subscribe to the raspberry/topic topic
+    # client.subscribe("IoTSolutions/#")
+    #client.subscribe("MK117-16d0/#")
+    client.subscribe("MK117-3e04/04/device_to_app/#")
+# the callback function, it will be triggered when receiving messages 
+def on_message(client, userdata, msg):
+    print(f" {msg.topic}")
+    print(f" {msg.payload}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+# set the will message, when the Raspberry Pi is powered off, or the network is interrupted abnormally, it will send the will message to other clients
+# client.will_set('raspberry/status', b'{"status": "Off"}')
+
+# create connection, the three parameters are broker address, broker port number, and keep-alive time respectively
+client.connect("broker.emqx.io", 1883, 60)
+
+# set the network loop blocking, it will not actively end the program before calling disconnect() or the program crash
+client.loop_forever()
